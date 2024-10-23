736b134634c0758e8b62843020bc5b5a8ab649b7
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2024-10-23 08:52:11 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int main(){
+    char pet;
+    printf("Do you have a cat or dog c/d ?");
+    scanf("%c\n",&pet);
+    if (pet == 'd' || pet == 'D'){
+        printf("Dogs say: hav\n");
+    }
+    else if (pet == 'c' || pet == "C"){
+        printf("cats say miyav\n");
+    }else{
+        printf("you entered wrong animal");
+    }
+    return 0;
+}
 
