1c092a6d5626d93f3b3592f03b635cec7c0980f6
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2024-10-23 08:50:42 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int guess;
+    int secret = 3;
+
+    printf("Guess a number between 1 and 5: ");
+    scanf("%d", &guess);
+
+   
+    if (guess == secret) {
+        printf("Congratulations! You guessed it right.\n");
+    } else {
+        printf("Sorry, wrong guess. The correct number was 3.\n");
+    }
+
+    return 0;
+}
 
