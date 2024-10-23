a9b95ca8a690ef1f27d67d3d1472027948ce2aed
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2024-10-23 08:51:07 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(void) {
+    int age;
+
+    
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+
+    for (int i = 1; i <= age; i++) {
+        printf("%d. Happy Birthday!\n", i);
+    }
+
+    return 0;
+}
